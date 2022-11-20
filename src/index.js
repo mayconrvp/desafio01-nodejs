14e4e954b36de1daa97d96179f36800ff14d185f
Maycon <maycon.rvp@outlook.com>
2022-11-20 00:31:15 -0300

    Inserido novos middlewares para verificações de teste

--- src/index.js
+++ src/index.js
@@ -25,17 +25,51 @@ function checksExistsUserAccount(request, response, next) {
   return next();
 }
 
-app.post('/users', (request, response) => {
-  const { name, username } = request.body;
+function checksExistsUsername(request, response, next) {
+  const { username } = request.body;
+  const user = users.find(user => user.username === username);
 
-  users.push({
+  if(user){
+    return response.status(400).json({
+      error: 'Mensagem do erro'
+    });
+  }
+
+  request.user = user;
+  return next();
+}
+
+function checksExistsTodo(request, response, next) {
+  const { id } = request.params;
+  const { user } = request;
+  let checkId = false;
+
+  user.todos.forEach(todo => {
+    if(todo.id == id){
+      checkId = true;
+    }
+  });
+
+  if(!checkId){
+    return response.status(400).json({
+      error: 'Non existing todo'
+    });
+  }
+
+  return next();
+}
+
+app.post('/users', checksExistsUsername, (request, response) => {
+  const { name, username } = request.body;
+  const user = {
     id: uuidv4(),
     name,
     username,
     todos: []
-  })
+  }
+  users.push(user);
 
-  return response.status(201).send();
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
@@ -48,18 +82,20 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { user } = request;
 
-  user.todos.push({
+  const todo = {
     id: uuidv4(),
     title,
     done: false,
     deadline: new Date(deadline),
     created_at: new Date()
-  });
+  };
 
-  return response.status(201).send();
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.put('/todos/:id', checksExistsUserAccount, checksExistsTodo, (request, response) => {
   const { user } = request;
   const { title, deadline } = request.body;
   const { id } = request.params;
@@ -71,10 +107,10 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
     }
   });
 
-  return response.status(201).send();
+  return response.status(200).send();
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+app.patch('/todos/:id/done', checksExistsUserAccount, checksExistsTodo, (request, response) => {
   const { user } = request;
   const { id } = request.params;
   const { done } = request.body;
@@ -85,7 +121,7 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
     }
   });
 
-  return response.status(201).send();
+  return response.status(200).send();
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
